b9227865246c2668fcadebc3352659def6b9fd6b
David Russell <druss337@gmail.com>
2024-11-03 11:40:47 -0700

    use express

--- scripts/auth-config.js
+++
@@ -1,5 +0,0 @@
-const auth0Config = {
-    domain: "YOUR_AUTH0_DOMAIN",
-    clientId: "YOUR_CLIENT_ID",
-    redirectUri: window.location.origin + "/index.html",  // Redirect after login
-};
--- scripts/auth0-sign-in.js
+++
@@ -1,8 +0,0 @@
-
-document.getElementById('login').addEventListener('click', async () => {
-    // const auth0 = await createAuth0Client(auth0Config);
-    const auth0 = await new Auth0Client(auth0Config);
-
-    // Redirect to Auth0 login page
-    await auth0.loginWithRedirect();
-});
